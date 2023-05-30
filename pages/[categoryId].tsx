eb0ff50e5ee6ea6300dd99141b52d4598ddf2628
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-05-30 17:27:26 +0800

    dfd

--- pages/[categoryId].tsx
+++ pages/[categoryId].tsx
@@ -8,36 +8,64 @@ export default function ClothesPage() {
   const { categoryId } = router.query;
 
   const [clothes, setClothes] = useState<any>([]);
+  async function fetchdata() {
+    const response = await axios.get(
+      `http://localhost:1337/api/blogs?populate[0]=category&filters[category][id][$eq]=${Number(
+        categoryId
+      )}`
+    );
+    const data = await response.data.data;
+    setClothes(data);
 
+    // axios
+    //   .get(
+    //     `http://localhost:1337/api/blogs?populate[0]=category&filters[category][id][$eq]=${Number(
+    //       categoryId
+    //     )}`
+    //   )
+    //   .then((res) => {
+    //     setClothes(res.data.data);
+    //   });
+  }
   useEffect(() => {
-    async function fetchdata() {
-      const response = await axios.get(
-        `http://localhost:1337/api/blogs?populate[0]=category&filters[category][id][$eq]=${Number(
-          categoryId
-        )}`
-      );
-      const data = await response.data.data;
-      setClothes(data);
-
-      // axios
-      //   .get(
-      //     `http://localhost:1337/api/blogs?populate[0]=category&filters[category][id][$eq]=${Number(
-      //       categoryId
-      //     )}`
-      //   )
-      //   .then((res) => {
-      //     setClothes(res.data.data);
-      //   });
+    if (categoryId && router.isReady) {
+      fetchdata();
     }
-    fetchdata();
-  }, []);
+  }, [categoryId]);
   console.log(clothes);
 
   return (
     <div>
       <NavbarPage />
       {clothes?.map((clothe: any) => {
-        return <p>{clothe.id}</p>;
+        return (
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              className="w-full"
+              src="/img/card-top.jpg"
+              alt="Sunset in the mountains"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{}</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
